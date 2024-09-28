872e8acb83ce11e04af1c270ab92479adbd9c33b
YaraGh <yaraesmaiel123@gmail.com>
2024-09-28 09:28:13 -0500

    Entity: Product - Category

---
+++ src/Abp.OnlineStore.Demo.Domain/Categories/Category.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Abp.OnlineStore.Demo.Categories
+{
+    public class Category : FullAuditedEntity<int>
+    {
+        public Category(string nameAr, string nameEn, string descriptionAr  , string descriptionEn ) 
+        {
+           
+            NameAr = nameAr;
+            NameEn = nameEn;
+            DescriptionAr = descriptionAr;
+            DescriptionEn = descriptionEn;
+        }
+
+        public string NameAr { get; set; }
+
+        public string NameEn { get; set; }
+
+        public string DescriptionAr { get; set; }
+
+        public string DescriptionEn { get; set; }
+    }
+}
---
+++ src/Abp.OnlineStore.Demo.Domain/Products/Product.cs
@@ -0,0 +1,25 @@
+﻿using Abp.OnlineStore.Demo.Categories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Abp.OnlineStore.Demo.Products
+{
+    public class Product : FullAuditedEntity<int>
+    {
+        public string NameAr { get; set; }
+
+        public string NameEn { get; set; }
+
+        public string DescriptionAr { get; set; }
+
+        public string DescriptionEn { get; set; }
+
+        public int CategoryId  { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
